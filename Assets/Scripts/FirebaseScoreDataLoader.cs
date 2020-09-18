4baaf2da3a8e6e481f269aef062ec9853e127898
Max <alterra.br@gmail.com>
2020-09-18 16:51:30 +0700

    Switch off firebase data loader

--- Assets/Scripts/FirebaseScoreDataLoader.cs
+++ Assets/Scripts/FirebaseScoreDataLoader.cs
@@ -1,60 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-using Firebase;
-using Firebase.Database;
-using System;
-using Newtonsoft.Json;
+//using Firebase;
+//using Firebase.Database;
+//using System;
+//using Newtonsoft.Json;
 
-public class FirebaseScoreDataLoader : IDataLoader
-{
-    public async void LoadScoreDataAsync(EventHandler<OnScoreDataLoadedEventArgs> callback)
-    {
-        OnScoreDataLoadedEventArgs args = null;
+//public class FirebaseScoreDataLoader : IDataLoader
+//{
+//    public async void LoadScoreDataAsync(EventHandler<OnScoreDataLoadedEventArgs> callback)
+//    {
+//        OnScoreDataLoadedEventArgs args = null;
 
-        await FirebaseDatabase.DefaultInstance.GetReference("Score").GetValueAsync().ContinueWith( task =>
-        {
-            if (task.IsCompleted)
-            {
-                Debug.Log(task.Result.GetRawJsonValue());
+//        await FirebaseDatabase.DefaultInstance.GetReference("Score").GetValueAsync().ContinueWith( task =>
+//        {
+//            if (task.IsCompleted)
+//            {
+//                Debug.Log(task.Result.GetRawJsonValue());
 
-                string json = task.Result.GetRawJsonValue();
+//                string json = task.Result.GetRawJsonValue();
 
-                // TODO: Use firebase to retrieve deserialized data
-                List<PlayerDataModel> data = null;
+//                // TODO: Use firebase to retrieve deserialized data
+//                List<PlayerDataModel> data = null;
 
-                try
-                {
-                    data = JsonConvert.DeserializeObject<List<PlayerDataModel>>(json);
-                }
-                catch (JsonSerializationException e)
-                {
-                    data = new List<PlayerDataModel>();
-                }
+//                try
+//                {
+//                    data = JsonConvert.DeserializeObject<List<PlayerDataModel>>(json);
+//                }
+//                catch (JsonSerializationException e)
+//                {
+//                    data = new List<PlayerDataModel>();
+//                }
 
-                args = new OnScoreDataLoadedEventArgs()
-                {
-                    status = ScoreLoadingStatus.Ok,
-                    errorMessage = "",
-                    data = data
-                };
-            }
-            else
-            {
-                args = new OnScoreDataLoadedEventArgs()
-                {
-                    status = ScoreLoadingStatus.DatabaseError,
-                    errorMessage = "",
-                    data = new List<PlayerDataModel>()
-                };
-            }
-        });
+//                args = new OnScoreDataLoadedEventArgs()
+//                {
+//                    status = ScoreLoadingStatus.Ok,
+//                    errorMessage = "",
+//                    data = data
+//                };
+//            }
+//            else
+//            {
+//                args = new OnScoreDataLoadedEventArgs()
+//                {
+//                    status = ScoreLoadingStatus.DatabaseError,
+//                    errorMessage = "",
+//                    data = new List<PlayerDataModel>()
+//                };
+//            }
+//        });
 
-        callback?.Invoke(this, args);
-    }
+//        callback?.Invoke(this, args);
+//    }
 
-    public async void SaveScoreEntryAsync(PlayerDataModel entry, Action callback)
-    {
-    }
-}
+//    public async void SaveScoreEntryAsync(PlayerDataModel entry, Action callback)
+//    {
+//    }
+//}
